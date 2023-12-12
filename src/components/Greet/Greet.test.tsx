49db2364590be6dde73974ded03785f47003d416
Mitesh2001 <miteshladva.dgrs@gmail.com>
2023-12-12 21:57:03 +0530

    video 13

--- src/components/Greet/Greet.test.tsx
+++ src/components/Greet/Greet.test.tsx
@@ -1,14 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import Greet from "./Greet";
 
-test("Greet renders correctly", () => {
-    render(<Greet />);
-    const textElement = screen.getByText('Hello');
-    expect(textElement).toBeInTheDocument();
+describe("Greet", () => {
+    test("Renders correctly", () => {
+        render(<Greet />);
+        const textElement = screen.getByText('Hello');
+        expect(textElement).toBeInTheDocument();
+    });
 });
 
-test("Greet name renders correctly", () => {
-    render(<Greet name="Mitesh" />);
-    const textElement = screen.getByText('Hello Mitesh');
-    expect(textElement).toBeInTheDocument();
+describe("Nested", () => {
+    test("Name renders correctly", () => {
+        render(<Greet name="Mitesh" />);
+        const textElement = screen.getByText('Hello Mitesh');
+        expect(textElement).toBeInTheDocument();
+    });
 });
